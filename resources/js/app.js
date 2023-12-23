248e74b3f4ce2977a2eeb6dcc593178e5690efe6
Underlyingglitch <rickokkersen@gmail.com>
2023-12-23 12:04:03 +0100

    OEPS

--- resources/js/app.js
+++ resources/js/app.js
@@ -16,6 +16,7 @@ window.Echo = new Echo({
     key: import.meta.env.VITE_PUSHER_APP_KEY,
     wsHost: import.meta.env.VITE_PUSHER_HOST,
     wsPort: import.meta.env.VITE_PUSHER_PORT,
+    wssPort: import.meta.env.VITE_PUSHER_PORT,
     cluster: import.meta.env.VITE_PUSHER_CLUSTER,
     forceTLS: false,
     encrypted: true,

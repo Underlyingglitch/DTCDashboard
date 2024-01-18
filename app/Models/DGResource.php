2a3432ba3095669a3f71475f2a996036a788d418
= <=>
2024-01-18 12:31:17 +0100

    Added link for deleted non files

--- app/Models/DGResource.php
+++ app/Models/DGResource.php
@@ -25,6 +25,9 @@ class DGResource extends Model
     public function getUrlAttribute()
     {
         if ($this->status == 'deleted') {
+            if ($this->type != 'file') {
+                return $this->attributes['url'];
+            }
             if (Storage::exists('dg_resources/' . $this->id . '.pdf')) {
                 return URL::signedRoute('dg_resources.download', ['dg_resource' => $this->id]);
             }

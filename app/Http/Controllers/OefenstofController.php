d439499280a1a4b7e9b93e2856232197a51d08f5
= <=>
2024-01-18 12:57:56 +0100

    Added out of date notification to oefenstof

--- app/Http/Controllers/OefenstofController.php
+++ app/Http/Controllers/OefenstofController.php
@@ -2,12 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\DGResource;
+use App\Models\Setting;
 use Illuminate\Http\Request;
 
 class OefenstofController extends Controller
 {
     public function index()
     {
-        return view('pages.oefenstof.index');
+        $last_updated = Setting::getValue('oefenstof_last_updated');
+        $up_to_date = DGResource::where('category', 'Oefenstof en reglement')->where(function ($query) use ($last_updated) {
+            $query->where('updated_at', '>', $last_updated)
+                ->orWhere('created_at', '>', $last_updated);
+        })->count() == 0;
+        return view('pages.oefenstof.index', [
+            'up_to_date' => $up_to_date,
+        ]);
     }
 }
--- resources/views/pages/oefenstof/index.blade.php
+++ resources/views/pages/oefenstof/index.blade.php
@@ -3,5 +3,17 @@
 @section('page_title', 'Oefenstof')
 
 @section('content')
+    @if (!$up_to_date)
+        <div class="alert alert-warning">
+            <p>
+                {{-- TODO: Add message if user is subscribed to email notifications on this subject --}}
+                De documenten op de website van Dutch Gymnastics zijn gewijzigd. Deze wijzigingen kunnen verschillen t.o.v.
+                de oefenstof op deze pagina. <b>De oefenstof van Dutch Gymnastics is altijd leidend.</b> We doen ons best om
+                deze pagina zo snel mogelijk bij te werken. Deze melding zal dan verdwijnen.
+            </p>
+            <a href="{{ route('dg_resources.index') }}">Bekijk deze wijzigingen</a>
+        </div>
+    @endif
 
+    
 @endsection

aea66eecaa2fd440ba7fde94acd26f1ba6d7f1e1
= <=>
2024-01-23 00:30:23 +0100

    Fixed problem with team score calculation

--- app/Jobs/CalculateTeamScore.php
+++ app/Jobs/CalculateTeamScore.php
@@ -34,11 +34,11 @@ class CalculateTeamScore implements ShouldQueue, ShouldBeUnique
      */
     public function handle(): void
     {
-        $scores = $this->team->registrations->pluck('scores')->flatten()->where('match_day_id', $this->match_day_id)->where('toestel', $this->toestel);
-        $topThreeScores = $scores->sortByDesc('total')->take(3);
+        $scores = $this->team->registrations->where('match_day_id', $this->match_day_id)->pluck('scores')->flatten()->where('match_day_id', $this->match_day_id)->where('toestel', $this->toestel);
+        $topScores = $scores->sortByDesc('total')->take(3);
         foreach ($scores as $score) {
             // Set counted to true if score is among the 3 highest scores for this toestel, otherwise set to false
-            $score->counted = $topThreeScores->contains($score);
+            $score->counted = $topScores->contains($score);
             $score->save();
         }
         // Sum all counted scores for this team on this toestel

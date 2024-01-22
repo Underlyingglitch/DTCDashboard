b61536919593652614285d7d22ca009be45f713d
= <=>
2024-01-22 14:48:04 +0100

    Updated import page

--- resources/js/import.js
+++ resources/js/import.js
@@ -2,12 +2,12 @@ import './bootstrap'
 
 $(() => {
     $('#type').on('change', () => {
-        if ($('#type').val() == 'registrations') {
-            $('#import_matchday_field').hide();
-            $('#file_field').show();
-        } else {
+        if ($('#type').val() == 'registrations_match') {
             $('#import_matchday_field').show();
             $('#file_field').hide();
+        } else {
+            $('#import_matchday_field').hide();
+            $('#file_field').show();
         }
     }).trigger('change');
 });
--- resources/views/pages/import/index.blade.php
+++ resources/views/pages/import/index.blade.php
@@ -5,8 +5,8 @@
 @section('content')
     <form action="{{ route('import.store') }}" method="post" enctype="multipart/form-data">
         @csrf
-        <x-form.select name="type" label="Type" :options="$import_options" :value="$type" />
         <x-form.select name="matchday" label="Wedstrijddag" :options="$matchdays" :value="$matchday" disabled="onvalue" />
+        <x-form.select name="type" label="Type" :options="$import_options" :value="$type" />
         <x-form.select name="import_matchday" label="Importeren van wedstrijddag" :options="$matchdays" />
         <x-form.file name="file" label="Bestand" />
         <input class="btn btn-primary" type="submit" value="Importeren" />

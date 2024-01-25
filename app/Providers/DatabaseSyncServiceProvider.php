e50c7f2faf80be8ca47b95b045b26e38bd012aeb
= <=>
2024-01-26 00:00:23 +0100

    Added database sync service provider

---
+++ app/Providers/DatabaseSyncServiceProvider.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Providers;
+
+use App\Models\Score;
+use App\Models\Setting;
+use App\Models\MatchDay;
+use App\Models\Wedstrijd;
+use App\Models\Competition;
+use App\Models\Registration;
+use App\Models\PendingChange;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\ServiceProvider;
+
+class DatabaseSyncServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        // If server is not production, do nothing
+        if (config('app.env') != 'local') {
+            return;
+        }
+        $models = [Competition::class, MatchDay::class, Wedstrijd::class, Registration::class, Score::class];
+        foreach ($models as $model) {
+            $model::creating(function ($model) {
+                if (request()->is('api/*')) {
+                    return true;
+                }
+                PendingChange::create([
+                    'model_type' => get_class($model),
+                    'model_id' => $model->id,
+                    'operation' => 'create',
+                    'data' => json_encode($model->toArray()),
+                ]);
+            });
+            $model::updating(function ($model) {
+                if (request()->is('api/*')) {
+                    return true;
+                }
+                PendingChange::create([
+                    'model_type' => get_class($model),
+                    'model_id' => $model->id,
+                    'operation' => 'update',
+                    'data' => json_encode($model->getDirty()),
+                ]);
+            });
+        }
+    }
+}
--- config/app.php
+++ config/app.php
@@ -170,6 +170,7 @@ return [
         App\Providers\RouteServiceProvider::class,
         App\Providers\ConstantsServiceProvider::class,
         App\Providers\DatabaseWriteServiceProvider::class,
+        App\Providers\DatabaseSyncServiceProvider::class,
         OwenIt\Auditing\AuditingServiceProvider::class,
     ])->toArray(),
 

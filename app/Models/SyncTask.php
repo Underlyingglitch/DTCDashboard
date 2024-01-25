088fe28cd1c61f8026ea46829c7e11eb8c99d6c8
Underlyingglitch <rickokkersen@gmail.com>
2024-01-26 00:35:14 +0100

    Updated synctask

--- app/Models/SyncTask.php
+++ app/Models/SyncTask.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class SyncTask extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'id',
+        'model_type',
+        'model_id',
+        'operation',
+        'data',
+        'synced'
+    ];
 }

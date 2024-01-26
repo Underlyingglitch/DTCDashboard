8ffcac0d949daad0d0e44e16bacafc6dba706f75
Underlyingglitch <rickokkersen@gmail.com>
2024-01-26 15:27:13 +0100

    Added check

--- app/Observers/SyncTasksObserver.php
+++ app/Observers/SyncTasksObserver.php
@@ -2,6 +2,7 @@
 
 namespace App\Observers;
 
+use App\Models\Setting;
 use App\Models\SyncTask;
 
 class SyncTasksObserver
@@ -9,6 +10,7 @@ class SyncTasksObserver
     public function created(SyncTask $st): void
     {
         // Check if score belongs to a team
+        if (Setting::getValue('sync_enabled') == 'false') return;
         event(new \App\Events\DataSync\UpdateSyncStatus(1));
     }
 }

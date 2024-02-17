1bcc7f675c1970febdcb1062aa06eb04b2df5946
Underlyingglitch <rickokkersen@gmail.com>
2024-02-17 03:37:30 +0100

    Added close button to disclaimer

--- resources/views/pages/livescores/index.blade.php
+++ resources/views/pages/livescores/index.blade.php
@@ -4,6 +4,9 @@
 
 @section('content')
     <div class="alert alert-info">
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
         <h4 class="alert-heading">Disclaimer</h4>
         <p>De scores op deze pagina kunnen vertraging hebben. In zeer uitzonderlijke gevallen kunnen de scores fouten
             bevatten. De gepubliceerde puntenlijsten zijn altijd leidend en aan deze scores kunnen geen rechten worden

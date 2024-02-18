035d34f8e54804294dfd11ad8fbd1e0982f16d13
= <=>
2024-02-18 21:35:41 +0100

    Added name field to MatchDay forms

--- resources/views/pages/matchdays/edit.blade.php
+++ resources/views/pages/matchdays/edit.blade.php
@@ -7,6 +7,7 @@
     <form method="post" action="{{ route('matchdays.update', $matchday) }}">
         @csrf
         @method('PUT')
+        <x-form.input name="name" label="Naam wedstrijddag" placeholder="Naam" :value="$matchday->name" />
         <x-form.date name="date" label="Datum wedstrijddag" placeholder="Datum" :value="$matchday->date" />
         <x-form.select name="location_id" label="Locatie wedstrijddag" :options="$locations" :value="$matchday->location_id" />
         <input class="btn btn-sm btn-primary" type="submit" value="Opslaan" />

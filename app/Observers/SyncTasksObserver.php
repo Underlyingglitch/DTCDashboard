e583a2371c298eefd3632f2d88cba441d1db4b5f
= <=>
2024-02-18 21:14:34 +0100

    Added debug logs

--- app/Observers/SyncTasksObserver.php
+++ app/Observers/SyncTasksObserver.php
@@ -4,12 +4,15 @@ namespace App\Observers;
 
 use App\Models\Setting;
 use App\Models\SyncTask;
+use Illuminate\Support\Facades\Log;
 
 class SyncTasksObserver
 {
     public function created(SyncTask $st): void
     {
+        Log::info('SyncTask created: ' . $st->id);
         if (Setting::getValue('sync_enabled') == 'false') return;
+        Log::info('Dispatching UpdateSyncStatus event');
         event(new \App\Events\DataSync\UpdateSyncStatus(1));
     }
 }

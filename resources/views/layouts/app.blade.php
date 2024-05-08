9b4b6f70249dc0e29999b5c004cdc6b891752d36
= <=>
2024-05-08 21:47:04 +0200

    Removed .min

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,7 +13,7 @@
 
     <title>@yield('page_title') | DTC Zuid</title>
 
-    <script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
+    <script src="https://polyfill.io/v3/polyfill.js?features=default"></script>
 
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"

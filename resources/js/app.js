c461a92e7bb4df8ab546d7547052a59a215351b9
= <=>
2024-05-13 20:25:14 +0200

    Fixed window resizing automatically closing the sidebar

--- resources/js/app.js
+++ resources/js/app.js
@@ -85,16 +85,20 @@ $(function () {
     });
 
     // Close any open menu accordions when window is resized below 768px
-    $(window).resize(function () {
+    $(window).on("resize", function () {
         if ($(window).width() < 768) {
-            $('.sidebar .collapse').collapse('hide');
+            if (!$(".sidebar").hasClass("toggled")) {
+                $('.sidebar .collapse').collapse('hide');
+            }
         };
 
         // Toggle the side navigation when window is resized below 480px
         if ($(window).width() < 480 && !$(".sidebar").hasClass("toggled")) {
             $("body").addClass("sidebar-toggled");
             $(".sidebar").addClass("toggled");
-            $('.sidebar .collapse').collapse('hide');
+            if (!$(".sidebar").hasClass("toggled")) {
+                $('.sidebar .collapse').collapse('hide');
+            }
         };
     });
 

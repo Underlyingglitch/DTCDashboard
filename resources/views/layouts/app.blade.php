473f9f76087da6445ecba83c46cab008445b099d
Underlyingglitch <rickokkersen@gmail.com>
2024-05-25 09:35:14 +0200

    Included font

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,9 +13,9 @@
 
     <title>@yield('page_title') | DTC Zuid</title>
 
-    <link
+    {{-- <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-        rel="stylesheet">
+        rel="stylesheet"> --}}
 
     <script>
         window.userId = @json(auth()->id());

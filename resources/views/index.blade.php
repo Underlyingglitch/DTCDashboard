b5bd1eafe26d8124f3ad3ef23f6ae1dee80499f6
= <=>
2024-06-01 12:44:12 +0200

    Removed external sources

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,10 +10,7 @@
     <title>DTC Zuid</title>
 
     <link rel="icon" href="{{ asset('images/icons/icon128x128.png') }}">
-
-    <link
-        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-        rel="stylesheet">
+    
     @vite(['resources/scss/app.scss', 'resources/js/app.js'])
 </head>
 
--- resources/views/pages/wedstrijden/scores/add.blade.php
+++ resources/views/pages/wedstrijden/scores/add.blade.php
@@ -68,9 +68,7 @@
         </form>
     </table>
     <a class="btn btn-primary" id="save-btn">Verwerken</a>
-    <script src="https://code.jquery.com/jquery-3.6.4.min.js"
-        integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=" crossorigin="anonymous"></script>
-    <script>
+    <script type="module">
         $(() => {
             $('#save-btn').on('click', function() {
                 let ids = []

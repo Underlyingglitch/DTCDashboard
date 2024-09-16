d4afe83074b43af342ff1a6e94583e3aae97e68d
Underlyingglitch <rickokkersen@gmail.com>
2024-09-17 01:14:14 +0200

    Added Proxy IP Header

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -19,6 +20,16 @@ return Application::configure(basePath: dirname(__DIR__))
             'locked' => \App\Http\Middleware\LockedAccount::class,
             'internalapi' => \App\Http\Middleware\CheckAPIToken::class
         ]);
+        $middleware->trustProxies(at: [
+            '10.244.0.0/16'
+        ]);
+        $middleware->trustProxies(
+            headers: Request::HEADER_X_FORWARDED_FOR |
+                Request::HEADER_X_FORWARDED_HOST |
+                Request::HEADER_X_FORWARDED_PORT |
+                Request::HEADER_X_FORWARDED_PROTO |
+                Request::HEADER_X_FORWARDED_AWS_ELB
+        );
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //

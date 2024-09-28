2fe939ff6b116f2c30c9a7c7533b1d980f536d1b
Underlyingglitch <36314703+Underlyingglitch@users.noreply.github.com>
2024-09-28 12:03:06 +0200

    Added debug mode for admin user only

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->appendToGroup('web', [
             \App\Http\Middleware\LastSeen::class,
+            \App\Http\Middleware\AdminDebug::class
         ]);
 
         $middleware->alias([

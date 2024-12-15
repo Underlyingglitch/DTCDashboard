7422a7514da20e3201f41d8255b4ffd1312472fc
Underlyingglitch <36314703+Underlyingglitch@users.noreply.github.com>
2024-12-15 20:51:19 +0100

    Calculating total e on save

--- app/Models/Score.php
+++ app/Models/Score.php
@@ -29,21 +29,21 @@ class Score extends Model implements Auditable
     {
         parent::boot();
 
-        // static::creating(function ($score) {
-        //     $es = array_filter([$score->e1, $score->e2, $score->e3]);
-        //     $score->e = count($es) > 0 ? round(array_sum($es) / count($es), 3) : null;
-        //     $total = $score->d > 0 ? (($score->d + (10 - $score->e)) - $score->n) : 0;
-        //     if ($total < 0) $total = 0;
-        //     $score->total = $total;
-        // });
+        static::creating(function ($score) {
+            $es = array_filter([$score->e1, $score->e2, $score->e3]);
+            $score->e = count($es) > 0 ? round(array_sum($es) / count($es), 3) : null;
+            $total = $score->d > 0 ? (($score->d + (10 - $score->e)) - $score->n) : 0;
+            if ($total < 0) $total = 0;
+            $score->total = $total;
+        });
 
-        // static::updating(function ($score) {
-        //     $es = array_filter([$score->e1, $score->e2, $score->e3]);
-        //     $score->e = count($es) > 0 ? round(array_sum($es) / count($es), 3) : null;
-        //     $total = $score->d > 0 ? (($score->d + (10 - $score->e)) - $score->n) : 0;
-        //     if ($total < 0) $total = 0;
-        //     $score->total = $total;
-        // });
+        static::updating(function ($score) {
+            $es = array_filter([$score->e1, $score->e2, $score->e3]);
+            $score->e = count($es) > 0 ? round(array_sum($es) / count($es), 3) : null;
+            $total = $score->d > 0 ? (($score->d + (10 - $score->e)) - $score->n) : 0;
+            if ($total < 0) $total = 0;
+            $score->total = $total;
+        });
     }
 
     public function match_day()

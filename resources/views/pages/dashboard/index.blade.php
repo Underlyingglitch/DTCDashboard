5f0d1a830a2bc6dab9c5c3d55c73b31eb12a5599
Underlyingglitch <36314703+Underlyingglitch@users.noreply.github.com>
2025-03-10 22:35:46 +0100

    Removed built in feedback widget

--- resources/views/pages/dashboard/index.blade.php
+++ resources/views/pages/dashboard/index.blade.php
@@ -37,36 +37,6 @@
             <div class="alert alert-primary">Deze website is nog in ontwikkeling. Mocht je een fout tegenkomen, laat dit mij
                 dan weten via <a href="mailto:rickokkersen@gmail.com">rickokkersen@gmail.com</a>, of gebruik het feedback
                 veld.</div>
-            <form action="{{ route('feedback.store') }}" method="post">
-                @csrf
-                <textarea class="form-control" name="feedback" placeholder="Laat hier uw feedback achter" cols="30" rows="5"></textarea>
-                <input type="submit" class="btn btn-primary" value="Insturen" />
-            </form>
-            @can('viewAll', \App\Models\Feedback::class)
-                <h4>Feedback</h4>
-
-                @foreach (\App\Models\Feedback::all() as $fb)
-                    <div class="card">
-                        <div class="card-header">
-                            <form action="{{ route('feedback.destroy', $fb) }}" method="post">
-                                {{ $fb->user->name }}
-                                @csrf
-                                @method('DELETE')
-                                <button type="submit"
-                                    style="float:right;color:red;background:none!important;border:none;padding:0!important;"><i
-                                        class="fas fa-trash"></i></button>
-                            </form>
-                        </div>
-                        <div class="card-body">
-                            {{ $fb->feedback }}<br>
-
-                        </div>
-                        <div class="card-footer">
-                            <small>{{ $fb->created_at->format('d-m-Y') }}</small>
-                        </div>
-                    </div>
-                @endforeach
-            @endcan
         </div>
     </div>
 @endsection

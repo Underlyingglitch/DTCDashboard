75ed8756f1f1a3fec6fae48856ba6b23d58d20fc
Underlyingglitch <36314703+Underlyingglitch@users.noreply.github.com>
2026-03-14 10:56:08 +0100

    Update juryblaadje

--- resources/views/pdf/jury.blade.php
+++ resources/views/pdf/jury.blade.php
@@ -9,28 +9,33 @@
                 @if ($group_nr[$key])
                     <table class="group-table" style="page-break-inside: avoid;">
                         <tr>
-                            <th colspan="3" class="group-name">{{ $wedstrijd->match_day->date->format('d-m-Y') }} |
-                                Wedstrijd
+                            <th colspan="2" class="group-name">{{ $wedstrijd->match_day->date->format('d-m-Y') }} |
+                                W
                                 {{ $wedstrijd->index }} | @if (count($baans) > 1)
-                                    Baan {{ $baan + 1 }} |
-                                @endif Groep {{ $group_nr[$key] - $baan * 10 }} |
-                                {{ $toestel }} | Ronde {{ $digital ? $i + 1 : count($group_nrs) - $i }}</th>
+                                    B {{ $baan + 1 }} |
+                                @endif G {{ $group_nr[$key] - $baan * 10 }} |
+                                {{ $toestel }} | R {{ $digital ? $i + 1 : count($group_nrs) - $i }}</th>
                             <th style="width: 80px">Niveau</th>
                             <th style="width: 50px">D-score</th>
-                            <th style="width: 50px">E-aftrek</th>
+                            <th style="width: 50px">E1-aftrek</th>
+                            <th style="width: 50px">E2-aftrek</th>
+                            <th style="width: 50px">E3-aftrek</th>
                             <th style="width: 50px">N-aftrek</th>
+                            <th style="width: 50px">Bonus</th>
                             <th style="width: 50px">Totaal</th>
                         </tr>
                         @foreach ($registrations->where('group_id', $group_nr[$key]) as $registration)
                             <tr
                                 @if ($registration->signed_off) style="text-decoration:line-through;text-decoration-thickness:2px" @endif>
                                 <td style="width: 20px">{{ $registration->startnumber }}</td>
-                                <td>{{ $registration->gymnast->name }}</td>
-                                <td style="width: 30%">{{ $registration->club->name }}</td>
+                                <td>{{ $registration->gymnast->name }} <br> {{ $registration->club->name }}</td>
                                 <td>{{ $registration->niveau->full_name }} @if ($registration->niveau->niveau_number)
                                         ({{ $registration->niveau->niveau_number }})
                                     @endif
                                 </td>
+                                <td>{{ $registration->signed_off ? '-' : '' }}</td>
+                                <td>{{ $registration->signed_off ? '-' : '' }}</td>
+                                <td>{{ $registration->signed_off ? '-' : '' }}</td>
                                 <td>{{ $registration->signed_off ? '-' : '' }}</td>
                                 <td>{{ $registration->signed_off ? '-' : '' }}</td>
                                 <td>{{ $registration->signed_off ? '-' : '' }}</td>
